50c274e2693b46a28966fa27d82885e23f2dd6e4
Chris Basinger <evilthreads669966@gmail.com>
2023-11-15 06:57:34 -0600

    clean up dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,10 +13,8 @@ repositories {
 dependencies {
     implementation("org.jetbrains.exposed:exposed-core:0.44.1")
     implementation("org.jetbrains.exposed:exposed-dao:0.44.1")
-    implementation("org.jetbrains.exposed:exposed-java-time:0.44.1")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.44.1")
     implementation("com.h2database:h2:2.1.214")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     testImplementation(kotlin("test"))
 }
 
